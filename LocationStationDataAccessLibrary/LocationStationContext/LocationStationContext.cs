fe19f54dd7cfe752e09ae40570b9033850d33a17
claude-newcombe <claudenewcombe94@gmail.com>
2020-06-25 15:52:14 +0100

    basic model and controller no database

---
+++ LocationStationDataAccessLibrary/LocationStationContext/LocationStationContext.cs
@@ -0,0 +1,14 @@
+﻿using LocationStationDataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocationStationDataAccessLibrary.LocationStationContext
+{
+    public class LocationStationContext : DbContext
+    {
+        public LocationStationContext(DbContextOptions options) : base(options) { }
+        public DbSet<MusicItem> MusicItems { get; set; }
+    }
+}
---
+++ LocationStationDataAccessLibrary/Models/MusicItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocationStationDataAccessLibrary.Models
+{
+    public class MusicItem
+    {
+        public int Id { get; set; }
+        public string SongName { get; set; }
+        public string Artist { get; set; }
+        public string LocationName { get; set; }
+        public int XCoordinate { get; set; }
+        public int YCoordinate { get; set; }
+        public string Metadata { get; set; }
+        public string YouTubeLink { get; set; }
+
+    }
+}
