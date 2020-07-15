f1d0aca68791eef1790487d62bbdb6e439ce83db
claude-newcombe <claudenewcombe94@gmail.com>
2020-07-15 16:43:46 +0100

    Multiple startup projects - location passed from JS to page model through form

---
+++ LocationStationWeb/Models/LocationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LocationStationWeb.Models
+{
+    public class LocationModel
+    {
+        [Required]
+        public string Longitude { get; set; }
+        [Required]
+        public string Latitude { get; set; }
+    }
+}
--- LocationStationWeb/Pages/Play.cshtml.cs
+++ LocationStationWeb/Pages/Play.cshtml.cs
@@ -5,26 +5,16 @@ using System.Threading.Tasks;
 using LocationStationWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace LocationStationWeb.Pages
 {
-    public class IndexModel : PageModel
+    public class PlayModel : PageModel
     {
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public LocationModel Location { get; set; }
         public void OnGet()
         {
 
-        }
-
-        public IActionResult OnPost()
-        {
-            if (ModelState.IsValid == false)
-            {
-                return Page();
-            }
-            return RedirectToPage("Play", Location);
         }
     }
 }
